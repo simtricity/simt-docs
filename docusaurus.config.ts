d4f76607110fea2f1b533cf02d6e48e8bc867825
Damon Rand <damon@simtricity.com>
2025-09-03 20:40:29 +0100

    fix: Configure custom domain for docs.simtricity.io
    - Update baseUrl to '/' for custom domain
    - Set url to https://docs.simtricity.io
    - Add CNAME file for GitHub Pages custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,10 +37,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://simtricity.github.io',
+  url: 'https://docs.simtricity.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/simt-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
